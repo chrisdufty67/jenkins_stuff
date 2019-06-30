aa46aa4e199030bb2bb45cf906d7ba7e946ae63b
root <root@Python.example.com>
2019-07-01 10:28:22 +1200

    4th commit

--- cdtest.sh
+++ cdtest.sh
@@ -3,6 +3,6 @@ count=0
 while [ $count -ne 10 ]
 do
 count=`expr $count + 1`
-echo "The count is $count"
+echo "The count is $count" >> /tmp/jenkins_run
 done
-echo "job Finished"
+echo "job Finished" > /tmp/jenkins_run
