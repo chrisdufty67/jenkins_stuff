116e6e1e6eba77caea5dfca4a59ecc74ba495913
root <root@Python.example.com>
2019-07-01 18:47:51 +1200

    11th commit

--- cdtest.sh
+++ cdtest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 count=0
-while [ $count -ne 200 ]
+while [ $count -ne 150 ]
 do
 count=`expr $count + 1`
-printf "The count is $count"
+echo "The count is $count"
 done
 echo "job Finished" 
